d0d38136a0c3533429485cb866c8f740266b50f4
Roberto Rodriguez <manufacturedba@gmail.com>
2017-01-11 16:18:24 -0700

    update tests

--- test/prime-factors.spec.js
+++ test/prime-factors.spec.js
@@ -21,5 +21,29 @@ define(function(require) {
     it('should return [3] for 3', function () {
       expect(primeFactors(3)).to.eql([3]);
     });
+
+    it('should return [2, 2] for 4', function () {
+      expect(primeFactors(4)).to.eql([2, 2]);
+    });
+
+    it('should return [5] for 5', function () {
+      expect(primeFactors(5)).to.eql([5]);
+    });
+
+    it('should return [2, 3] for 6', function () {
+      expect(primeFactors(6)).to.eql([2, 3]);
+    });
+
+    it('should return [2, 2, 2] for 8', function () {
+      expect(primeFactors(8)).to.eql([2, 2, 2]);
+    });
+
+    it('should return [3, 3] for 9', function () {
+      expect(primeFactors(9)).to.eql([3, 3]);
+    });
+
+    it('should return [2, 2, 5, 5] for 100', function () {
+      expect(primeFactors(100)).to.eql([2, 2, 5, 5]);
+    });
   });
 })
