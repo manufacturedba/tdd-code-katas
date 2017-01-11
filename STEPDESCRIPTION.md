7ec960d93d91f4074d5d40e09e0d319ada563cc9
Roberto Rodriguez <manufacturedba@gmail.com>
2017-01-11 15:29:35 -0700

    Gut test/source file and add description file

---
+++ STEPDESCRIPTION.md
@@ -0,0 +1,1 @@
+0. Setting up our module to test and add canary test
--- src/prime-factors.js
+++ src/prime-factors.js
@@ -1,21 +1,5 @@
 define([], function() {
-  return function primeFactors(number) {
-    if (isNaN(number)) {
-      throw new TypeError('Invalid parameter: Only parameters of type "number" allowed');
-    }
-
-    if (number % 1 !== 0 || number < 0) {
-      throw new Error('Invalid parameter: Only positive integers allowed');
-    }
-
-    var primes = [];
-
-    for (var candidate = 2; number > 1; candidate++) {
-      for (; number % candidate == 0; number /= candidate) {
-        primes.push(candidate);
-      }
-    }
-
-    return primes;
+  return function primeFactors() {
+    
   };
 });
