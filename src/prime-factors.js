ef20f0020b1e5c42bea529eae4f8d3a88bc2aaf9
Roberto Rodriguez <manufacturedba@gmail.com>
2017-01-11 15:18:14 -0700

    feat: Switch to AMD syntax

--- src/prime-factors.js
+++ src/prime-factors.js
@@ -1,19 +1,21 @@
-module.exports = function primeFactors(number) {
-  if (isNaN(number)) {
-    throw new TypeError('Invalid parameter: Only parameters of type "number" allowed');
-  }
-
-  if (number % 1 !== 0 || number < 0) {
-    throw new Error('Invalid parameter: Only positive integers allowed');
-  }
-
-  var primes = [];
-
-  for (var candidate = 2; number > 1; candidate++) {
-    for (; number % candidate == 0; number /= candidate) {
-      primes.push(candidate);
+define([], function() {
+  return function primeFactors(number) {
+    if (isNaN(number)) {
+      throw new TypeError('Invalid parameter: Only parameters of type "number" allowed');
     }
-  }
 
-  return primes;
-};
+    if (number % 1 !== 0 || number < 0) {
+      throw new Error('Invalid parameter: Only positive integers allowed');
+    }
+
+    var primes = [];
+
+    for (var candidate = 2; number > 1; candidate++) {
+      for (; number % candidate == 0; number /= candidate) {
+        primes.push(candidate);
+      }
+    }
+
+    return primes;
+  };
+});
