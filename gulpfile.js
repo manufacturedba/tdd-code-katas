89078a59d49c13b5d5f29cdb6fa9531c8f57538e
Roberto Rodriguez <manufacturedba@gmail.com>
2017-01-11 15:18:56 -0700

    chore(gulp): Gut Mocha set-up and replace with Karma tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,30 @@
 var gulp = require('gulp');
-var mocha = require('gulp-mocha');
+var Server = require('karma').Server;
 var notifierReporter = require('mocha-notifier-reporter');
-var istanbul = require('gulp-istanbul');
 
-notifierReporter.icon_pass = __dirname + '/img/success.png';
-notifierReporter.icon_fail = __dirname + '/img/fail.png';
-notifierReporter.message_pass = 'Congratulations. You are better than everyone.';
-notifierReporter.message_fail = 'The cake is a lie.';
-
-gulp.task('coverage', function () {
-  return gulp.src('src/**/*.js')
-    .pipe(istanbul({includeUntested: true}))
-    .on('finish', function () {
-      gulp.src('test/**/*.spec.js')
-        .pipe(mocha({reporter: 'spec'}))
-        .pipe(istanbul.writeReports())
-        .pipe(istanbul.enforceThresholds({ thresholds: { global: 75 } }));
-    });
+gulp.task('test', function(done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true,
+    reporters: ['progress', 'notify'],
+    notifyReporter: {
+      reportEachFailure: true, // Default: false, Will notify on every failed sepc
+      reportSuccess: false, // Default: true, Will notify when a suite was successful
+    }
+  }, done).start();
 });
 
-gulp.task('test', function() {
-  gulp.src(['test/**/*.spec.js'])
-    .pipe(mocha({
-      options: 'tdd',
-      reporter: notifierReporter.decorate('spec')
-    }));
-});
-
-gulp.task('tdd', ['test'], function() {
-  return gulp.watch(['test/**/*.spec.js', 'src/**/*.js'], ['test']);
+gulp.task('tdd', function(done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: false,
+    autoWatch: true,
+    reporters: ['progress', 'notify'],
+    notifyReporter: {
+      reportEachFailure: true, // Default: false, Will notify on every failed sepc
+      reportSuccess: false, // Default: true, Will notify when a suite was successful
+    }
+  }, done).start();
 });
 
 gulp.task('default', ['tdd']);
