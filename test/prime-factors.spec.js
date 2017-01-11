7a410cc7218df64957726b041a15c7df1e2fd657
Roberto Rodriguez <manufacturedba@gmail.com>
2017-01-11 15:41:53 -0700

    Update tests to fail

--- test/prime-factors.spec.js
+++ test/prime-factors.spec.js
@@ -1,8 +1,25 @@
 define(function(require) {
+  var primeFactors = require('../src/prime-factors');
 
   describe('primeFactors()', function() {
     it('should pass the canary test', function() {
       expect(false).to.be.false;
+    });
+
+    it('should return an empty array for 0', function () {
+      expect(primeFactors(0)).to.be.empty;
+    });
+
+    it('should return an empty array for 1', function () {
+      expect(primeFactors(1)).to.be.empty;
+    });
+
+    it('should return [2] for 2', function () {
+      expect(primeFactors(2)).to.eql([2]);
+    });
+
+    it('should return [3] for 3', function () {
+      expect(primeFactors(3)).to.eql([3]);
     });
   });
 })
